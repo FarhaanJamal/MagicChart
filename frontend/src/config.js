0fccf628f536dd558469c1cbc4296d1a117136a7
Farhaan <95125546+FarhaanJamal@users.noreply.github.com>
2025-01-01 20:39:02 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:8000/api"
-//export const BASE_URL = "https://magicchart-backend.onrender.com/api"
+//export const BASE_URL = "http://localhost:8000/api"
+export const BASE_URL = "https://magicchart-backend.onrender.com/api"
 export const token = localStorage.getItem("token")
